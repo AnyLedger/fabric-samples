5c71876bbc9b348e73ad76876ebf63f8704fce81
Bogdan Djukic <bdjukic@gmail.com>
2018-07-18 16:43:00 +0200

    Enabling the deployment of Device Manager chaincode to HyperLedger for basic-network example.

--- basic-network/start.sh
+++ basic-network/start.sh
@@ -24,3 +24,5 @@ sleep ${FABRIC_START_TIMEOUT}
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel create -o orderer.example.com:7050 -c mychannel -f /etc/hyperledger/configtx/channel.tx
 # Join peer0.org1.example.com to the channel.
 docker exec -e "CORE_PEER_LOCALMSPID=Org1MSP" -e "CORE_PEER_MSPCONFIGPATH=/etc/hyperledger/msp/users/Admin@org1.example.com/msp" peer0.org1.example.com peer channel join -b mychannel.block
+# Deploy chaincode
+docker exec cli peer chaincode install -n device_manager -v 1.0 -p github.com/contracts
